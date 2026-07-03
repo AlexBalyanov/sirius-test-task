85ac3576df09051e90ba92d06d9b198f5fe926c0
Alex Balyanov <alex44298@yandex.ru>
2026-07-03 19:26:11 +0500

    wip: добавил radix popup + стили

--- src/components/ChipList/ChipList.tsx
+++ src/components/ChipList/ChipList.tsx
@@ -1,5 +1,11 @@
 import styles from "./ChipList.module.css";
 import Chip from "../Chip/Chip.tsx";
+import {
+  Popover,
+  PopoverContent,
+  PopoverPortal,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
 
 export type TChipListItem = {
   id: string;
@@ -13,20 +19,57 @@ type TChipListProps = {
 };
 
 const ChipList = (props: TChipListProps) => {
-  const { items, selectedIds, onSelect } = props;
+  const { items, selectedIds = [], onSelect } = props;
+
+  const visibleCount = 5;
+  const visibleItems = items.slice(0, visibleCount);
+  const hiddenItems = items.slice(visibleCount);
 
   return (
     <ul className={styles.list}>
-      {items.map((item) => (
+      {visibleItems.map((item) => (
         <li key={item.id} className={styles.item}>
           <Chip
-            selected={selectedIds?.includes(item.id)}
+            selected={selectedIds.includes(item.id)}
             onClick={() => onSelect(item.id)}
           >
             {item.label}
           </Chip>
         </li>
       ))}
+      {hiddenItems.length > 0 && (
+        <li className={styles.moreItem}>
+          <Popover>
+            <PopoverTrigger asChild>
+              <button className={styles.moreButton} type="button">
+                ...
+              </button>
+            </PopoverTrigger>
+
+            <PopoverPortal>
+              <PopoverContent
+                className={styles.popup}
+                align="end"
+                side="bottom"
+                sideOffset={8}
+              >
+                <ul className={styles.popupList}>
+                  {hiddenItems.map((item) => (
+                    <li key={item.id}>
+                      <Chip
+                        selected={selectedIds.includes(item.id)}
+                        onClick={() => onSelect(item.id)}
+                      >
+                        {item.label}
+                      </Chip>
+                    </li>
+                  ))}
+                </ul>
+              </PopoverContent>
+            </PopoverPortal>
+          </Popover>
+        </li>
+      )}
     </ul>
   );
 };
